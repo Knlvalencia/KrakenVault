4cb3aeaeef5f18144a299c220629bf433929c80e
Knlvalencia <knlvalencia01202401014@usep.edu.ph>
2026-04-13 20:27:44 +0800

    changed database config

--- config.php
+++ config.php
@@ -1,7 +1,8 @@
 <?php
-define('DB_HOST', 'db.xffhfyebobldkisphhgf.supabase.co');
+define('DB_HOST', 'db.gxnuzudnibtvisvecekq.supabase.co');
 define('DB_PORT', '5432');
 define('DB_NAME', 'postgres');
 define('DB_USER', 'postgres');
-define('DB_PASS', 'n05Ep68lkPZsMHBt');
+define('DB_PASS', 'kylegwapokaayo123');
 ?>
+
